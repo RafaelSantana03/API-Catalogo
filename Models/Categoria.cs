41643a05ea36d70df0623f68913900138e5a2092
Rafael Santana <rafa03santana@gmail.com>
2025-12-12 08:09:22 -0300

    First commit

---
+++ Models/Categoria.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace APICatalogo.Models;
+public class Categoria
+{
+    public int CategoriaId { get; set; } // O entity framework vai indentificar essa chave como primaria
+    public string? Nome { get; set; }
+    public string? ImagemUrl { get; set; } 
+
+}
+
+
