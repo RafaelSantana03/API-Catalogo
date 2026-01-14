43d8de63a346d34c74a9524522d1cd1c00cef0e4
Rafael Santana <rafa03santana@gmail.com>
2026-01-13 22:27:23 -0300

    Definindo o relacionamento entre as entidades

--- Models/Categoria.cs
+++ Models/Categoria.cs
@@ -1,11 +1,19 @@
-﻿using System.Reflection.Metadata.Ecma335;
+﻿using System.Collections.ObjectModel;
+using System.Reflection.Metadata.Ecma335;
 
 namespace APICatalogo.Models;
 public class Categoria
 {
+
+    public Categoria()
+    {
+        Produtos = new Collection<Produto>();
+    }
     public int CategoriaId { get; set; } // O entity framework vai indentificar essa chave como primaria
     public string? Nome { get; set; }
-    public string? ImagemUrl { get; set; } 
+    public string? ImagemUrl { get; set; }
+
+    public ICollection<Produto>? Produtos { get; set; }
 
 }
 
