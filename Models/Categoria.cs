a8ffcf0fb067ef4b138c56edff602baf5b3d6d8b
Rafael Santana <rafa03santana@gmail.com>
2026-01-29 22:12:54 -0300

    Aplicando o Data Annotations

--- Models/Categoria.cs
+++ Models/Categoria.cs
@@ -1,16 +1,25 @@
 ﻿using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Reflection.Metadata.Ecma335;
-
 namespace APICatalogo.Models;
+
+[Table("Categoria")]
 public class Categoria
 {
-
     public Categoria()
     {
         Produtos = new Collection<Produto>();
     }
+    [Key]
     public int CategoriaId { get; set; } // O entity framework vai indentificar essa chave como primaria
+
+    [Required]
+    [StringLength(80)]
     public string? Nome { get; set; }
+
+    [Required]
+    [StringLength(300)]
     public string? ImagemUrl { get; set; }
 
     public ICollection<Produto>? Produtos { get; set; }
