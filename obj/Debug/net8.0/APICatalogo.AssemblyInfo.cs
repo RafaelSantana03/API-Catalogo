71273396cdb4e0209e994063d7ea8768627e6894
Rafael Santana <rafa03santana@gmail.com>
2026-02-10 21:24:51 -0300

    otimizando o codigo da API

--- obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
+++ obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+452b04135dbb82432a7a09497a68af1718a24bd1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b540d589225fa6024ddf13e24ecb737cf48dea7f")]
 [assembly: System.Reflection.AssemblyProductAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
