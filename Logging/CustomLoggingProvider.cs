2141f095e94007cd4bb3686f56659afb483eb1be
Rafael Santana <rafa03santana@gmail.com>
2026-02-24 08:06:59 -0300

    criando um logging customizado

---
+++ Logging/CustomLoggingProvider.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Concurrent;
+
+namespace APICatalogo.Logging
+{
+    public class CustomLoggingProvider : ILoggerProvider
+    {
+        readonly CustomLoggerProviderConfiguration loggerConfig;
+
+        readonly ConcurrentDictionary<string, CustomerLogger> loggers =
+            new ConcurrentDictionary<string, CustomerLogger>();
+
+        public CustomLoggingProvider(CustomLoggerProviderConfiguration config)
+        {
+            loggerConfig = config;
+        }
+
+        public ILogger CreateLogger(string categoryName)
+        {
+            return loggers.GetOrAdd(categoryName, name => new CustomerLogger(name, loggerConfig));
+        }
+
+        public void Dispose()
+        {
+            loggers.Clear();
+        }
+    }
+}
