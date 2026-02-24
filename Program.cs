e854a48f63a431eb1d80227d6ad3fee0a052caf4
Rafael Santana <rafa03santana@gmail.com>
2026-02-24 08:21:30 -0300

    Otimizando o codigo do projeto

--- Program.cs
+++ Program.cs
@@ -10,10 +10,10 @@ using System.Text.Json.Serialization;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllers()
-    .AddJsonOptions(options =>
-    options.JsonSerializerOptions
-    .ReferenceHandler = ReferenceHandler.IgnoreCycles);
+builder.Services.AddControllers(options =>
+{
+    options.Filters.Add(typeof(ApiExceptionFilter));
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
--- obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
+++ obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+280ef41f3e5b2baad7d567994c30e147f8953ec6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36a02545d4d8d1b8baa6c01022f57ce88b86fb20")]
 [assembly: System.Reflection.AssemblyProductAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
