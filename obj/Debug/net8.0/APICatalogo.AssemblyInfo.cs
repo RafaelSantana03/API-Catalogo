c77306a3e5de89b02084c14445989fce7e762cb2
Rafael Santana <rafa03santana@gmail.com>
2026-03-04 22:45:03 -0300

    implementando o Repositorio generico II

--- obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
+++ obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f151555faa8c399e912ecdc4c94b098ecbe68193")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+653d56493c512f8f24b9dadebe807297bc2972e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
