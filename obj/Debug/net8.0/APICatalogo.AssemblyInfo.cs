4f76d53e4b86f9934b8a93ee6060aebafc4a5342
Rafael Santana <rafa03santana@gmail.com>
2026-03-17 08:17:23 -0300

    DTO - Aplicando metodos de extensao

--- obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
+++ obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf533f9953bee9fe9821792b979a85a8e9f55431")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af69d072e0f09d2da15acc7b06ee4524a8b9ae5f")]
 [assembly: System.Reflection.AssemblyProductAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
